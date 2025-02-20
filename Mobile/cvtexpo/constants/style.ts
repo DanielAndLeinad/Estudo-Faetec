d6b99e3e707b33c4604136c30ad0a4199d7a489f
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-02-20 20:52:13 -0300

    !

---
+++ Mobile/cvtexpo/constants/style.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+//Exportando um objeto com as propriedades de estilo
+const style = StyleSheet.create({
+    centralizado: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
