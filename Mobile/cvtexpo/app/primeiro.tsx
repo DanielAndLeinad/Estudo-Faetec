6c834c0301f8fbbb96ace682dc78c044eba63f85
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-02-20 21:14:13 -0300

    !

--- Mobile/cvtexpo/app/primeiro.tsx
+++ Mobile/cvtexpo/app/primeiro.tsx
@@ -1,8 +1,9 @@
 //Importanando Text e View do react-native
-import { Text, View } from "react-native";
+import { Text, View, } from "react-native";
+import styles from "../constants/styles";
 export default function primeiro() {
   return (
-    <View>
+    <View style = {styles.centralizado}>
       <Text> Primeiro Componente </Text>
     </View>
   );
--- Mobile/cvtexpo/constants/styles.ts
+++ Mobile/cvtexpo/constants/styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
 //Exportando um objeto com as propriedades de estilo
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     centralizado: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
 });
+
+export default styles;
