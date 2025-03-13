2db11bec86a6f53d0179953f8787be012c1ad2a2
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-03-13 20:16:27 -0300

    !

---
+++ Mobile/aulas/constants/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    centralizado:{
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center'
+    }
+});
+
+export default styles
