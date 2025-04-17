2d9e3342a13091d205acca968c820feced043df0
Daniel <145384176+DanielAndLeinad@users.noreply.github.com>
2025-04-16 21:22:08 -0300

    !

---
+++ Mobile/form/src/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; 
+import styles from '../constants/styles';
+
+export default function Index() {
+  return (
+    <View style={[styles.centralizado, { gap: 10 }]}>
+      <Ionicons name="logo-react" size={100}/>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={{ fontSize: 20, fontWeight: '700' }}>React Native</Text>
+      </View>
+    </View>
+  );
+}
